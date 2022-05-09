3cf4306108fb2cf18c9da71db335939e95fad70b
Jordan S <jpstanway@gmail.com>
2022-05-08 23:25:57 -0700

    create canvas and title

--- public/game.mjs
+++ public/game.mjs
@@ -1,6 +1,12 @@
-import Player from './Player.mjs';
-import Collectible from './Collectible.mjs';
+import Player from "./Player.mjs";
+import Collectible from "./Collectible.mjs";
 
-const socket = io();
-const canvas = document.getElementById('game-window');
-const context = canvas.getContext('2d');
+// const socket = io();
+const canvas = document.getElementById("game-window");
+const context = canvas.getContext("2d");
+
+context.fillStyle = "black";
+context.fillRect(0, 0, canvas.width, canvas.height);
+context.font = "32px sans-serif";
+context.fillStyle = "white";
+context.fillText("Crypto Race", 220, 40);
