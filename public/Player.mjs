33f49dcc3ebb6f574e81dd118475725912a9ee06
Jordan S <jpstanway@gmail.com>
2022-05-15 16:49:35 -0700

    determine boundaries for game board

--- public/Player.mjs
+++ public/Player.mjs
@@ -9,11 +9,31 @@ class Player {
     this.score = score;
   }
 
-  movePlayer(dir, speed) {}
+  movePlayer(dir, speed) {
+    if (dir) {
+      const [x, y] = this.currentPosition;
+      if (dir === "up") {
+        this.currentPosition = [x, y - 1];
+      }
+      if (dir === "down") {
+        this.currentPosition = [x, y + 1];
+      }
+      if (dir === "left") {
+        this.currentPosition = [x - 1, y];
+      }
+      if (dir === "right") {
+        this.currentPosition = [x + 1, y];
+      }
+    }
+  }
 
   collision(item) {}
 
   calculateRank(arr) {}
+
+  position() {
+    return this.currentPosition;
+  }
 }
 
 export default Player;
--- public/game.mjs
+++ public/game.mjs
@@ -2,6 +2,14 @@ import Player from "./Player.mjs";
 import Collectible from "./Collectible.mjs";
 
 const PADDING = 40;
+const HEADER_HEIGHT = 70;
+const PLAYER_SIZE = 20;
+const DIRECTIONS = {
+  87: "up", // w
+  65: "left", // a
+  83: "down", // s
+  68: "right", // d
+};
 
 // const socket = io();
 const canvas = document.getElementById("game-window");
@@ -30,21 +38,42 @@ context.strokeStyle = "white";
 context.lineWidth = 2;
 context.beginPath();
 context.moveTo(0, 70);
-context.lineTo(canvas.width, 70);
+context.lineTo(canvas.width, HEADER_HEIGHT);
 context.stroke();
 
 // spawn character
 const spawnPlayer = () => {
   const id = String(Math.floor(Math.random() * 100));
-  const x = Math.floor(Math.random() * (canvas.width + 1));
-  const y = Math.floor(Math.random() * (canvas.width - 70) + 70);
+  const maxX = canvas.width - PLAYER_SIZE;
+  const minX = PLAYER_SIZE;
+  const maxY = canvas.height - PLAYER_SIZE;
+  const minY = HEADER_HEIGHT + PLAYER_SIZE;
+  const x = Math.floor(Math.random() * (maxX - minX) + minX);
+  const y = Math.floor(Math.random() * (maxY - minY) + minY);
   const score = 0;
-  const player = new Player({ x, y, score, id });
 
   // draw new player
   context.beginPath();
-  context.arc(x, y, 20, 0, Math.PI * 2, true);
+  context.arc(x, y, PLAYER_SIZE, 0, Math.PI * 2, true);
+  context.stroke();
+
+  return new Player({ x, y, score, id });
+};
+const player = spawnPlayer();
+
+// player movement
+canvas.focus();
+const movementHandler = (e) => {
+  const dir = DIRECTIONS[e.which];
+  player.movePlayer(dir, 1);
+  const [x, y] = player.position();
+  context.translate(x, y);
+  context.strokeStyle = "white";
+  context.stroke();
+
+  context.setTransform(1, 0, 0, 1, 0, 0);
+
+  context.strokeStyle = "black";
   context.stroke();
 };
-
-spawnPlayer();
+canvas.addEventListener("keydown", movementHandler);
